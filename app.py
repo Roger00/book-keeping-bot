da56d8dc79318723f2fb23bedeb0fc3a34e46be1
Roger00 <roger00.h@gmail.com>
2024-09-21 13:33:29 +0800

    :wrench: read channel access token from app config vars

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, abort
 
 from linebot import (
@@ -11,9 +13,9 @@ from linebot.models import *
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 # Channel Secret
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
