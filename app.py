15456876d496dafe8583b7b341d2354434c8c3e6
Roger00 <roger00.h@gmail.com>
2024-09-21 16:13:11 +0800

    use openai to analyze user input in bot app

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from linebot.models import *
 
 from sheet import append_booking
 from booking import Booking, datetime_str
+from bot import analyze
 
 app = Flask(__name__)
 
@@ -32,12 +33,13 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     original_message = event.message.text
-    append_booking(Booking(datetime=datetime_str(), main_cat='食品酒水', sub_cat='午餐', expense=7, income=0, description=original_message))
-    message = TextSendMessage(text=done_message(event.message.text))
-    line_bot_api.reply_message(event.reply_token, message)
+    for b in analyze(original_message):
+        append_booking(b)
+        message = TextSendMessage(text=done_message(b))
+        line_bot_api.reply_message(event.reply_token, message)
 
-def done_message(message):
-    return f'已記帳: {message}'
+def done_message(booking):
+    return f'已記帳: {booking.main_cat} - {booking.sub_cat}: {booking.description} ${booking.expense}'
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
