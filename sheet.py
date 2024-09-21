3584cafc667e08b56444f51366b2e2904b7b9e27
Roger00 <roger00.h@gmail.com>
2024-09-21 15:09:59 +0800

    :wrench: read credentials from env

--- sheet.py
+++ sheet.py
@@ -1,3 +1,4 @@
+import os
 import gspread
 from collections import namedtuple
 from typing import NamedTuple
@@ -6,8 +7,22 @@ Booking = namedtuple('Booking', 'datetime, main_cat, sub_cat, expense, income, d
 
 SPREADSHEET_ID = '1xBp1arSbr-qX4t0tQirVtvjdMgR5_R4-WoIezaoquyM'
 
+credentials = {
+  "type": "service_account",
+  "project_id": f"{os.environ['GOOGLE_API_PROJECT_ID']}",
+  "private_key_id": f"{os.environ['GOOGLE_API_PRIVATE_KEY_ID']}",
+  "private_key": f"{os.environ['GOOGLE_API_PRIVATE_KEY']}".replace('\\n', '\n'),
+  "client_email": f"{os.environ['GOOGLE_API_CLIENT_EMAIL']}",
+  "client_id": f"{os.environ['GOOGLE_API_CLIENT_ID']}",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/book-keeping-bot%40fishintosky.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com",
+}
+
 def append_booking(b: NamedTuple):
-    gc = gspread.service_account()
+    gc = gspread.service_account_from_dict(credentials)
     wks = gc.open_by_key(SPREADSHEET_ID).sheet1
     wks.append_row([b.datetime, '', b.main_cat, b.sub_cat, b.expense, b.income, b.description], table_range="A1:G1")
 
