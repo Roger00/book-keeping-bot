a293399dc49b3618cfea6b9de4e10d6f9a20c0de
Roger00 <roger00.h@gmail.com>
2024-09-21 19:53:16 +0800

    :bug: force utc+8 timezone

--- booking.py
+++ booking.py
@@ -4,8 +4,8 @@ from datetime import datetime, timezone, timedelta
 Booking = namedtuple('Booking', 'datetime, main_cat, sub_cat, expense, income, description')
 
 def datetime_str():
-    now = datetime.now()
+    now = datetime.now(timezone(offset=timedelta(hours=8)))
     am_pm = "上午" if now.hour < 12 else "下午"
     hour_12 = now.hour if now.hour <= 12 else now.hour - 12
-    formatted_time = now.astimezone(timezone(offset=timedelta(hours=8))).strftime(f"%Y/%-m/%d {am_pm} {hour_12}:%M:%S")
+    formatted_time = now.strftime(f"%Y/%-m/%d {am_pm} {hour_12}:%M:%S")
     return formatted_time
